c0a339c55f561b0b2ddfff1c982fbf704b847aed
Othman Al Ghandour <othmanghandour51@gmail.com>
2024-09-10 20:45:06 +0300

    Add blog posts retrive update destroy class to path url

--- blogproject/api/urls.py
+++ blogproject/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import BlogPostListCreate
+from .views import BlogPostListCreate, BlogPostRetriveUpdateDestroyAPIView
 urlpatterns = [
-    path('blogposts/',  BlogPostListCreate.as_view(), name='blogpost-list-create')
+    path('blogposts/',  BlogPostListCreate.as_view(), name='blogpost-list-create'),
+    path('blogposts/<int:pk>/', BlogPostRetriveUpdateDestroyAPIView.as_view(), name='blogpost-get-update-delete')
 ]
